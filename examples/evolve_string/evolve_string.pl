95482fdb3603f8b46f8cfd6dfebf8f06a2b1ff86
Christian Theil Have <cth@ruc.dk>
2009-11-05 14:07:57 +0100

    Some bug fixes and making the example work.

--- examples/evolve_string/evolve_string.pl
+++ examples/evolve_string/evolve_string.pl
@@ -5,14 +5,17 @@
 
 :- ['levenshtein']. % We use Stephan Maiers edit distance implementation for fitness.
 
-target_string("genetic algorithms rock").
-characters("abcdefghijklmnopqrstuvwxyz ").
+target_string("abcdef").
+%characters("abcdefghijklmnopqrstuvwxyz ").
+characters("abcdef").
 point_mutation_probability(0.1).
 
 run_example :-
-	population_size(20), % The number of individuals in each generation
-	mutation_rate(0.1),
+	population_size(50), % The number of individuals in each generation
+	mutation_rate(0.5),
 	cross_over_rate(0.2),
+	fitness_threshold(0),
+	generation_threshold(1000),
 	selection_mode(elitism), % Survisors are selected using elitism scheme
 	phase(initialization). % Start the algorithm
 
@@ -23,17 +26,18 @@ run_example :-
 % This rule is called every time a new individual should be created 
 % for inclusion in the initial generation. It will be invoked as many 
 % times as specified by population_size
-/*cb_create_individual(Genome) :-
-	create_random_string(Genome).
-*/
-cb_create_individual("netic algerock smogtir").
+cb_create_individual(Genome) :-
+	create_random_string(Genome),
+	write('cb_create_individual: '), atom_codes(A,Genome), write(A),nl.
+
+%cb_create_individual("netic algerock smogtir").
 	
 cb_calculate_fitness(Genome,Fitness) :-
-	write('calculate fitness'),nl,
 	target_string(Target),
 	atom_codes(Atom1,Target),
 	atom_codes(Atom2,Genome),
-	levenshtein(Atom1,Atom2,Fitness).
+	levenshtein(Atom1,Atom2,Distance),
+	Fitness is 0 - Distance.
 
 % Creates random point mutation 
 cb_mutate([],[]).
@@ -41,21 +45,38 @@ cb_mutate([C|GenomeRest], [M|MutatedGenomeRest]) :-
 	random(Outcome),
 	point_mutation_probability(P),
 	((Outcome =< P) -> create_random_character(M) ; M=C),
-	random_mutate(GenomeRest,MutatedGenomeRest).
+	cb_mutate(GenomeRest,MutatedGenomeRest).
 
-cb_cross_over(Genome1,Genome2) :-
-	first_half(Genome1,FirstHalf),
-	last_half(Genome1,LastHalf),
-	append(FirstHalf,LastHalf,Genome2).
+cb_cross_over(GenomeFather,GenomeMother,ChildGenome) :-
+	length(GenomeFather, FLen),
+	length(GenomeMother, MLen),
+	random(R1),
+	random(R2),
+	FChars is round(R1*FLen),
+	MChars is round(R2*MLen),
+	from_start(FChars,GenomeFather,FatherAllele),
+	from_end(MChars,GenomeMother,MotherAllele),
+	append(FatherAllele,MotherAllele,ChildGenome).
 	
-t :- 
-	cb_create_individual(G),
-	write(G),nl,
+t :-
+	cb_cross_over([z,x,z,x,z,x,z,x,z,x,z,x,z,x,z,x,z,x,z,x],[a,b,c,d,e,f,g,h,i,j,k,l,m,n],Child),
+	atom_codes(Str,Child),
+	write(Str),nl,
 	t.
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Various utility rules used by cb_ rules
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+from_start(0,_,[]).
+from_start(N,[C|Rest1],[C|Rest2]) :-
+	N1 is N - 1,
+	from_start(N1,Rest1,Rest2).
+	
+from_end(N,In,Out) :-
+	reverse(In,RevIn),
+	from_start(N,RevIn,RevOut),
+	reverse(RevOut,Out).
 
 % Get the first half of a list
 first_half(Str, HalfStr) :-
@@ -87,6 +108,6 @@ create_random_character(Char) :-
 	characters(Chars),
 	random(P),
 	length(Chars,NumChars),
-	CharIndexFloat is P * NumChars-1,
+	CharIndexFloat is P * (NumChars-1),
 	round(CharIndexFloat, CharIndex),
 	nth0(CharIndex,Chars,Char).
--- genetichr.pl
+++ genetichr.pl
@@ -1,36 +1,37 @@
 :- use_module(library(chr)).
-:- chr_constraint phase/1, generation/1, individual/3,  mutate/1, cross_over/2, uniq_id/1,
+:- chr_constraint phase/1, generation/1, individual/4,  mutate/1, cross_over/2, uniq_id/1,
 	population_size/1, population_counter/1, collect_generation/1, sorted_survivors/2,
-	mutation_rate/1, cross_over_rate/1, selection_mode/1.
+	mutation_rate/1, cross_over_rate/1, selection_mode/1,
+	fitness_threshold/1, generation_threshold/1, best_individual/1, average_fitness/1,
+	report_on_cycle/0.
 
 % Assign unique identifier to a new child
-individual(newly_born,Genome,Fitness) \ uniq_id(Id) <=>
+individual(new_id,Generation,Genome,Fitness), uniq_id(Id) <=>
 	NextId is Id + 1,
-	uniq_id(NextId),
-	individual(Id,Genome,Fitness).
+	individual(Id,Generation,Genome,Fitness),
+	uniq_id(NextId).
 
 % Fitness calculation: The user is expected to have implemented cb_calculate_fitness/2 which 
 % must return a number representing the fitness of the individual (higher = more fit).
 calculate_fitness @
-individual(Id,Genome,fitness_unknown) <=> 
+individual(Id,Generation,Genome,fitness_unknown) <=>
 	cb_calculate_fitness(Genome,Fitness),
-	individual(Id,Genome,Fitness).
+	individual(Id,Generation,Genome,Fitness).
 
 % Crossover: Certain individuals are selected for cross over. The user is expected to implement
 % the cb_cross_over/3 rule, where the third argument must unify with 
 cross_over @
-individual(Id1,Genome1,_), individual(Id2, Genome2,_) \ cross_over(Id1,Id2) <=>
-	cb_cross_over(Genome1,Genome2,ChildGenome)
-	|
-	individual(new_id,ChildGenome,fitness_unknown).
+generation(G), individual(Id1,_,Genome1,_), individual(Id2,_,Genome2,_) \ cross_over(Id1,Id2) <=>
+	cb_cross_over(Genome1,Genome2,ChildGenome),
+	NextGeneration is G + 1,
+	individual(new_id,NextGeneration,ChildGenome,fitness_unknown).
 	
 % Mutation: Inviduals are selected for mutation with a probability defined by the mutation rate
 % The user is expected to implement the cb_mutate/2 which mutates a genome. 
 mutate @
-mutate(Id), individual(Id,Genome,_) <=>
-	cb_mutate(Genome,MutatedGenome)
-	|
-	individual(Id,MutatedGenome,fitness_unknown).
+mutate(Id), individual(Id,Generation,Genome,_) <=>
+	cb_mutate(Genome,MutatedGenome),
+	individual(Id,Generation,MutatedGenome,fitness_unknown).
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Phase control
@@ -39,40 +40,66 @@ mutate(Id), individual(Id,Genome,_) <=>
 phase(initialization), population_size(S) ==> generation(0), uniq_id(1), population_counter(S).
 
 % When all initial individuals have been created, then start mutatation phase
-phase(initialization), population_counter(0), generation(X) <=> 
-	write('Generation '), write(X),nl,
+generation(G) \ phase(initialization), population_counter(0) <=> 
+	write('Generation '), write(G),nl,
 	phase(mutation).
 
+% Create new individuals
 phase(initialization) \ population_counter(PopSize) <=>
-	cb_create_individual(Genome),
-	write('created new individual: '), write(Genome),nl
+	cb_create_individual(Genome)
+	%write('created new individual: '), write(Genome),nl
 	|
 	NewPopSize is PopSize - 1,
-	population_size(NewPopSize),
-	individual(newly_born,Genome,fitness_unknown).
-	
+	individual(new_id,0,Genome,fitness_unknown),
+	population_counter(NewPopSize).
+
 phase(mutation) ==>	write('  - mutation phase'),nl.
 
-phase(mutation), mutation_rate(Rate), individual(Id,_,_) ==>
+phase(mutation), mutation_rate(Rate), individual(Id,_,_,_) ==>
 	random(Number),
-	Number >= Rate
+	Number =< Rate
 	|
+%	write('mutating '), write(Id), nl,
 	mutate(Id).
 
-phase(mutation) <=> write('  - cross over phase'), nl, phase(cross_over).
+phase(mutation) <=> write('  - cross over phase'),nl, phase(cross_over).
 
-phase(cross_over), cross_over_rate(Rate), individual(Id1,_,_), individual(Id2,_,_) ==>
+generation(G), phase(cross_over), cross_over_rate(Rate), individual(Id1,G1,_,_), individual(Id2,G2,_,_) ==>
+	Id1 < Id2,
+	%write(G), write(' '), write(G1), write(' '), write(G2), nl,
+	G >= G1, G >= G2, % Do not cross-over children from this generation
 	random(Number),
-	Number >= Rate
+	Number =< Rate*Rate
 	|
+%	write('crossing over '), write(Id1), write(' and '), write(Id2),nl,
 	cross_over(Id1,Id2).
 	
 phase(cross_over) <=> write('  - selection phase'), nl, phase(selection).
 
-generation(X), phase(evolution_cycle_done) <=> 
+% Report best individual and average fitness each evolutionary cycle
+individual(Id,Generation,Genome,Fitness) \ report_on_cycle, best_individual(Id), average_fitness(MeanFit) <=>
+	write('Best individual: , '), write(individual(Id,Generation,Genome,Fitness)), nl,
+	write('Average fitness of generation: '), write(MeanFit), nl.
+
+% Termination because we found a good enough individual
+best_individual(Id), individual(Id,_,_,Fitness) \ generation(_), phase(evolution_cycle_done), fitness_threshold(Threshold) <=>
+	Fitness >= Threshold
+	|
+	write('Individual with fitness threshold '),write(Threshold),write(' found. Terminating.'),nl,
+	report_on_cycle.
+
+% Termination because with reached the maximal generation threshold
+generation(Threshold), phase(evolution_cycle_done), generation_threshold(Threshold) <=>
+	write('Reached generation threshold'), write(Threshold),	write('. Terminating.'),nl,
+	report_on_cycle.
+
+% Evolutionary cycle done - proceeed to next cycle.	
+generation(X) \ phase(evolution_cycle_done) <=> 
 	Y is X + 1,
-	write('Evolution cycle done. Staring on generation '),write(Y),nl,sleep(1), 
-	chr_show_store(''),
+	write('Evolution cycle done. Starting on generation '),write(Y),nl,
+%	chr_show_store(''),
+	report_on_cycle,
+%	sleep(1), 	
 	generation(Y),
 	phase(mutation).
 	
@@ -84,12 +111,18 @@ generation(X), phase(evolution_cycle_done) <=>
 
 phase(selection), selection_mode(elitism) ==> collect_generation([]).
 
-collect_generation(L), individual(Id,Genome,Fitness) <=>
-	collect_generation([individual(Id,Genome,Fitness)|L]).
+collect_generation(L), individual(Id,Generation,Genome,Fitness) <=>
+	collect_generation([individual(Id,Generation,Genome,Fitness)|L]).
 
 phase(selection), selection_mode(elitism), population_size(PopSize) \ collect_generation(Generation) <=>
 	fitness_sort(Generation,Sorted),
-	sorted_survivors(PopSize, Sorted).
+	Sorted = [individual(BestId,_,_,_)|_],
+	sum_fitness(Sorted,TotalFitness),
+	length(Sorted, TotalIndividuals),
+	MeanFit is TotalFitness / TotalIndividuals,
+	average_fitness(MeanFit),
+	best_individual(BestId),
+	sorted_survivors(PopSize, Sorted).	
 
 sorted_survivors(NumLeft,[Indvidual|Rest]) <=> 
 	NumLeft > 0
@@ -98,7 +131,7 @@ sorted_survivors(NumLeft,[Indvidual|Rest]) <=>
 	Indvidual,
 	sorted_survivors(NextNumLeft,Rest).
 
-sorted_survivors(0, _) <=> phase(evolution_cycle_done).
+phase(selection), sorted_survivors(0, _) <=> phase(evolution_cycle_done).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % End of evolutionary cycle
@@ -113,6 +146,12 @@ phase(evolution_cycle_done), generation(X) <=>
 % Utility functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+sum_fitness([],0).
+sum_fitness([individual(_,_,_,Fit)|Rest],SumFit) :-
+	sum_fitness(Rest, SumFitRest),
+	SumFit is Fit + SumFitRest.
+
+
 fitness_sort(List,Sorted):-q_sort(List,[],Sorted).
 q_sort([],Acc,Acc).
 q_sort([H|T],Acc,Sorted):-
@@ -120,5 +159,5 @@ q_sort([H|T],Acc,Sorted):-
 	q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted).
 
 pivoting(_,[],[],[]).
-pivoting(H,[X|T],[X|L],G) :- H=individual(_,_,FitH), X=individual(_,_,FitX), FitX=<FitH, pivoting(H,T,L,G).
-pivoting(H,[X|T],L,[X|G]):- H=individual(_,_,FitH), X=individual(_,_,FitX), FitX>FitH, pivoting(H,T,L,G).
+pivoting(H,[X|T],[X|L],G) :- H=individual(_,_,_,FitH), X=individual(_,_,_,FitX), FitX=<FitH, pivoting(H,T,L,G).
+pivoting(H,[X|T],L,[X|G]):- H=individual(_,_,_,FitH), X=individual(_,_,_,FitX), FitX>FitH, pivoting(H,T,L,G).
